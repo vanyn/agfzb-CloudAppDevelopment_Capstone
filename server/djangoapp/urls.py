530358dc442a5c888783bc57d562c891f639ec23
tomvan <tomvan.dev@gmail.com>
2023-11-08 15:11:36 -0500

    wk2 changes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,16 +16,16 @@ urlpatterns = [
     path('about/', views.about, name='about'),
 
     # path for contact us view
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
 
     # path for registration
-    #path('registration/', views.registration_request, name='registration'),
+    path('registration/', views.registration_request, name='registration'),
 
     # path for login
-    #path('login/', views.login_request, name='login'),
+    path('login/', views.login_request, name='login'),
 
     # path for logout
-    #path('logout/', views.logout_request, name='logout'),
+    path('logout/', views.logout_request, name='logout'),
 
     # path for dealer reviews view
     #path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
